fc178a6f1002a749d8cd089717e305692c4326e9
Igor Nosach <igornosach23@gmail.com>
2017-10-12 14:46:20 +0200

    Final commit.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 
 public class Main {
+
+
     public static void main(String[] args) {
 
         try {
@@ -13,18 +15,21 @@ public class Main {
             SiteStatistics statistics2 = new QuoraApi("John-Romero");
 
             System.out.println("Olivers statistics of " + " answers:" + statistics1.getNumberAnswers()); //125
+
             System.out.println("Olivers statistics of " + " questions:" + statistics1.getNumberQuestions()); //17
 
             System.out.println("Johnys statistics of " + " answers:" + statistics2.getNumberAnswers());//98
+
             System.out.println("Johnys statistics of " + " questions:" + statistics2.getNumberQuestions());//0
 
             System.out.println(statistics1.getJSONObject()); //...
-            System.out.println(statistics2.getJSONObject());
 
-        } catch (IOException e) {
+            System.out.println(statistics2.getJSONObject()); //...
+
+        } catch (IOException | JSONException |NumberFormatException e) {
+
             System.out.println(e.getMessage());
-        } catch (JSONException e) {
-            e.printStackTrace();
+
         }
     }
 }
--- src/main/java/QuoraApi.java
+++ src/main/java/QuoraApi.java
@@ -18,22 +18,30 @@ public class QuoraApi implements SiteStatistics {
     public QuoraApi(String nameUser) throws IOException {
 
         this.nameUser = nameUser;
+
         setNumberAnswers(parseUsersAnswers());
+
         setNumberQuestions(parseUsersQuestions());
 
     }
 
 
     public int getNumberAnswers() {
+
         return numberAnswers;
+
     }
 
     private void setNumberAnswers(int numberAnswers) {
+
         this.numberAnswers = numberAnswers;
+
     }
 
     public int getNumberQuestions() {
+
         return numberQuestions;
+
     }
 
     private void setNumberQuestions(int numberQuestions) throws IOException {
@@ -42,11 +50,15 @@ public class QuoraApi implements SiteStatistics {
     }
 
     public int getNumberUpvotes() {
+
         return numberUpvotes;
+
     }
 
     private void setNumberUpvotes(int numberUpvotes) {
+
         this.numberUpvotes = numberUpvotes;
+
     }
 
     public String getNameUser() {
@@ -55,38 +67,58 @@ public class QuoraApi implements SiteStatistics {
     }
 
     private void setNameUser(String nameUser) {
+
         this.nameUser = nameUser;
+
     }
 
     private int parseUsersAnswers() throws IOException {
+
         Pattern findAnswers = Pattern.compile("(\\d+)( Answers)");
+
         Matcher m = findAnswers.matcher(parseUsersInformation().text());
+
         m.find(); // may be IllegalStateException
+
         final int answers = Integer.parseInt(m.group(1));
+
         return answers;
 
     }
 
     private int parseUsersQuestions() throws IOException {
+
         Pattern findAnswers = Pattern.compile("(\\d+)( Questions)");
+
         Matcher m = findAnswers.matcher(parseUsersInformation().text());
+
         m.find(); //may be IllegalStateException
+
         final int formKey = Integer.parseInt(m.group(1));
+
         return formKey;
     }
 
 
     private Element parseUsersInformation() throws IOException {
+
         final Document initial = Jsoup.connect("https://www.quora.com/" + getNameUser()).get();
+
         final Element infUsers = initial.getElementsByClass("horizontal_scroll_inner").first();
+
         return infUsers;
     }
 
     public JSONObject getJSONObject() throws JSONException {
+
        final JSONObject jsonObject = new JSONObject();
+
         jsonObject.put("name",getNameUser());
+
         jsonObject.put("answers",getNumberAnswers());
+
         jsonObject.put("questions",getNumberQuestions());
+
         return jsonObject;
     }
 }
