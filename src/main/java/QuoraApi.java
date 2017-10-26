87eb0104156d3f88ca5032f631cdcd21964a72f1
Igor <31211943+IgorNosach23@users.noreply.github.com>
2017-10-26 13:58:45 +0200

    Update QuoraApi.java

--- src/main/java/QuoraApi.java
+++ src/main/java/QuoraApi.java
@@ -101,7 +101,9 @@ public class QuoraApi implements Api {
     @Override
     public void authenticate(String userName, String password) throws IOException {
         if (userName == null || userName.isEmpty()) {
+            
             throw new IOException("Incorrect name!");
+            
         }
         setNameUser(userName);
 
@@ -110,87 +112,19 @@ public class QuoraApi implements Api {
         setNumberQuestions(parseUsersQuestions());
     }
 
-    private void setupWebDriver()
-    {
-
-        String os = System.getProperty("os.name");
-        if (os .contains("Windows") ) {
-            System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
-            System.setProperty("webdriver.phantomjs.driver", "./lib/lib/phantomjs.exe");
-            System.setProperty("webdriver.opera.driver", "./lib/launcher.exe");
-
-        }
-        if (os .contains("Mac")) {
-            try {
-                System.setProperty("webdriver.chrome.driver", "./lib/chromedriver");
-            }
-            catch (Exception e) {
-                System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.dmg");
-            }
-        }
-        WebDriver driver = null;
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
-
-
-            ChromeOptions options = new ChromeOptions();
-            try {
-                options.addArguments(new String[] { "--start-maximized" });
-            } catch (Exception e) {}
-
-
-
-/* 102 */       ChromeOptions options2 = new ChromeOptions();
-/*     */
-/* 104 */         options2.addArguments(new String[] { "--start-maximized" });
-/*     */
-
-
-
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setJavascriptEnabled(true);
-
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "./lib/lib/phantomjs.exe");
-
-        driver = new PhantomJSDriver(caps);
-      //  driver.manage().window().setSize(new Dimension(1000,800));
-
-            driver.get("https://www.quora.com/" + getNameUser());
-
-            /*
-       WebElement loginWindow = driver.findElement(By.name("email"));
-      loginWindow.click();
-       loginWindow.sendKeys(new CharSequence[] {"igornosach23@gmail.com"});
-
-       WebElement password = driver.findElement(By.name("password"));
-        loginWindow.click();
-       password.sendKeys(new CharSequence[]{"Next622521"});
-*/
-
-
-      // WebElement element = driver.findElement(By.xpath("//*[@value='Login' AND @class='submit_button ignore_interaction' AND @tabindex='4']"));
-        List<WebElement> element = driver.findElements(By.xpath("//span[@class='button_text']"));
-
-        element.forEach((WebElement w)-> System.out.println(w.getText()));
-
-
-
-
-
-
-
-
-
-
-    }
-
     @Override
     public String reputationJson() throws IOException, JSONException {
+        
         JSONObject jsonObject = new JSONObject();
+        
         jsonObject.put("answers", getNumberAnswers());
+        
         jsonObject.put("questions", getNumberQuestions());
+        
         return jsonObject.toString();
+        
     }
-
+    
     @Override
     public void logOut() throws IOException {
 
