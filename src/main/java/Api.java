40d4e515e4873cdab96bac44725258cd0bd191ac
Igor <31211943+IgorNosach23@users.noreply.github.com>
2018-02-07 11:18:02 +0200

    Update Api.java

--- src/main/java/Api.java
+++ src/main/java/Api.java
@@ -3,11 +3,7 @@ import org.json.JSONException;
 import java.io.IOException;
 
 public interface Api {
-
     void authenticate(String userName, String password) throws IOException;
-
     String reputationJson() throws IOException, JSONException;
-
     void logOut() throws IOException;
-
 }
