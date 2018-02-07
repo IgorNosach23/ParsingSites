0fba12d0550959995fb54c9f0f615f3410a75009
Igor <31211943+IgorNosach23@users.noreply.github.com>
2018-02-07 11:17:43 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,27 +5,16 @@ import java.io.IOException;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException, JSONException {
-
         Api quora = new QuoraApi();
-
         quora.authenticate("Igor", "Lazarev");
-
         Api stackOverFlow = new StackOverFlowApi();
-
         stackOverFlow.authenticate("---", "---");
-
         System.out.println(stackOverFlow.reputationJson());
-
         System.out.println(quora.reputationJson());
-
         stackOverFlow.logOut();
-
         quora.logOut();
-
     }
-
 }
 
 
